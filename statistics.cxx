915698f00e6e985643aa9afbbfaab31f9000ab68
Kratzradio <maria_kersten@yahoo.de>
2015-11-16 11:41:09 +0100

    hw3

--- statistics.cxx
+++ statistics.cxx
@@ -3,16 +3,33 @@
 #include <cstdlib>
 
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
+
+void f(double* p, const int N){
+	for(int i=0; i<N; i++){
+	int rand();
+	p[i] = rand()*1.0/RAND_MAX*1.0;
+	}
+
+}
+void g(double& mean, double& var, const int N, double* p){
+	mean = 0;
+		for(int i=0; i<N; i++){
+		mean += p[i]/N;
+		}
+	var = 0;
+		for(int i=0; i<N; i++){
+		var += pow(p[i]-mean,2)/N;
+		}
+}
 
 int main(){
-   const int N = 100;
+srand(time(NULL));
+   const int N = 1000;
    double p[N];
    double mean, var;
 
-   // Some lines here....
+	f(p, N);
+	g(mean, var, N, p);
 
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
